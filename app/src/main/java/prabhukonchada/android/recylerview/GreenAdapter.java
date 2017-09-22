0eb27109f05af0064936f3b0328a3bd30066f65e
Prabhu Konchada <prabhukonchada@gmail.com>
2017-09-22 12:23:05 +0530

    Created a project to experiment with recyler views and am using boiler plate code to get started

---
+++ app/src/main/java/prabhukonchada/android/recylerview/GreenAdapter.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package prabhukonchada.android.recylerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * We couldn't come up with a good name for this class. Then, we realized
+ * that this lesson is about RecyclerView.
+ *
+ * RecyclerView... Recycling... Saving the planet? Being green? Anyone?
+ * #crickets
+ *
+ * Avoid unnecessary garbage collection by using RecyclerView and ViewHolders.
+ *
+ * If you don't like our puns, we named this Adapter GreenAdapter because its
+ * contents are green.
+ */
+public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHolder> {
+
+    private static final String TAG = GreenAdapter.class.getSimpleName();
+
+    private int mNumberItems;
+
+    public GreenAdapter(int numberOfItems) {
+        mNumberItems = numberOfItems;
+    }
+
+    @Override
+    public NumberViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.number_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        NumberViewHolder viewHolder = new NumberViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(NumberViewHolder holder, int position) {
+        Log.d(TAG, "#" + position);
+        holder.bind(position);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
+
+
+    class NumberViewHolder extends RecyclerView.ViewHolder {
+
+        TextView listItemNumberView;
+
+        public NumberViewHolder(View itemView) {
+            super(itemView);
+            listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+        }
+
+        void bind(int listIndex) {
+            listItemNumberView.setText(String.valueOf(listIndex));
+        }
+    }
+}
